cc19fd5d059d20be175c1bf088ea56e440d921c3
Kelsey McBratney <kelseymcbratney@gmail.com>
2023-09-18 14:56:01 -0600

    test compile changes

--- PA1/NgramMapReduce.java
+++ PA1/NgramMapReduce.java
@@ -129,8 +129,9 @@ public class NgramMapReduce extends Configured implements Tool {
       System.exit(res); // res will be 0 if all tasks are executed succesfully and 1 otherwise
     }
 
+    @Override
     public int run(String[] args) throws Exception {
-      Configuration conf = this.getConf();
+      Configuration conf = getConf();
       Profiles profiles[] = { Profiles.A1, Profiles.A2, Profiles.B1, Profiles.B2 };
       for (Profiles p : profiles) {
         conf.setEnum("profile", p);
